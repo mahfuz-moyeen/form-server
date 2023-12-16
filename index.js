8604c2aa7b2506c4c93339f4fa6a07f010894647
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2023-12-17 03:00:43 +0600

    deploy server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+
 const sectorsCollection = require("./data/sectorsdata.json");
-const userDataCollection = require("./data/userData.json");
+let userDataCollection = require("./data/userData.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,44 +10,47 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-async function run() {
-  try {
-    // get sectors
-    app.get("/sectors", async (req, res) => {
-      res.send(sectorsCollection);
-    });
 
-    // get form
-    app.get("/form", async (req, res) => {
-      res.send(userDataCollection);
-    });
 
-    //update form
-    app.patch("/form-update", async (req, res) => {
-      const data = req.body;
-      userDataCollection.name = data.name;
-      userDataCollection.sector = data.sector;
-      userDataCollection.terms = data.terms;
-      res.send({ message: "Form updated successfully" });
-    });
-
-    //delete  form
-    app.delete("/form-clear", async (req, res) => {
-      userDataCollection.name = "";
-      userDataCollection.sector = "";
-      userDataCollection.terms = false;
-      res.send({ message: "form clear successfully" });
-    });
-  } finally {
-  }
-}
-
-run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("form server on");
+// Get sectors
+app.get("/sectors", (req, res) => {
+  res.send(sectorsCollection);
 });
 
+// Get form
+app.get("/form", (req, res) => {
+  res.send(userDataCollection);
+});
+
+// Update form
+app.patch("/form-update", async (req, res) => {
+  const data = req.body;
+  try {
+    userDataCollection.name = data.name;
+    userDataCollection.sector = data.sector;
+    userDataCollection.terms = data.terms;
+
+    res.send({ message: "Form updated successfully" });
+  } catch (error) {
+    console.error("Error updating form:", error.message);
+    res.status(500).json({ error: `Internal Server Error: ${error.message}` });
+  }
+});
+
+// Delete form
+app.delete("/form-clear", async (req, res) => {
+  userDataCollection.name = "";
+  userDataCollection.sector = "";
+  userDataCollection.terms = false;
+  res.send({ message: "Form cleared successfully" });
+});
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Form server is running");
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log("listening port :", port);
+  console.log("Server is listening on port:", port);
 });
